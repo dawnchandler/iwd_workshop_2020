381e25d7534499eec0640cfe0b579eef97f504b7
Dawn Chandler <dawnstellachandler@gmail.com>
2020-03-02 23:08:56 -0800

    Add password generator program.

---
+++ generator.py
@@ -0,0 +1,22 @@
+'''
+Basic program to generate a password.
+Inspired by: https://www.lastpass.com/password-generator
+'''
+
+import random
+
+# Get the desired length of password:
+passLen = input('Enter password length: ')
+passLen = int(passLen)
+
+# Pool of characters for password:
+chars = 'abcdefghijklmnopqrstuvwxyz0123456789!@#$%ˆ&*()'
+charsLen = len(chars)
+
+# Build the password by adding random characters:
+password = ''
+for _ in range(passLen):
+    i = random.randint(0, charsLen - 1)
+    password += chars[i]
+    
+print('Your password is: ' + password)
