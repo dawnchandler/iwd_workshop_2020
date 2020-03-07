68139cd3ea3b3516a04cd49b981f783fa387b4e8
Dawn Chandler <dawnstellachandler@gmail.com>
2020-03-07 01:06:27 -0800

    Edited import statement, simplified concat syntax.

--- generator.py
+++ generator.py
@@ -3,7 +3,7 @@ Basic program to generate a password.
 Inspired by: https://www.lastpass.com/password-generator
 '''
 
-import random
+from random import randint
 
 # Get the desired length of password:
 passLen = input('Enter password length: ')
@@ -16,7 +16,7 @@ charsLen = len(chars)
 # Build the password by adding random characters:
 password = ''
 for _ in range(passLen):
-    i = random.randint(0, charsLen - 1)
-    password += chars[i]
+    i = randint(0, charsLen - 1)
+    password = password + chars[i]
     
 print('Your password is: ' + password)
